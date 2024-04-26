970a0d7663eed49417d8a7550ec034b639c7f5b5
Atilla Karagöz <109253064+AtillaKaragoz@users.noreply.github.com>
2024-04-26 21:03:25 +0300

    proje dosyaları atıldı

--- randevuOtomasyonu/obj/Debug/net8.0/randevuOtomasyonu.AssemblyInfo.cs
+++ randevuOtomasyonu/obj/Debug/net8.0/randevuOtomasyonu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("randevuOtomasyonu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4e384284568f3e1a050e5f10aa132d2941f916")]
 [assembly: System.Reflection.AssemblyProductAttribute("randevuOtomasyonu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("randevuOtomasyonu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
